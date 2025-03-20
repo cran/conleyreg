7fab177b0b41386bbcb4e11d1354da57ae0cbb52
Christian Düben <cdueben.ml+cran@proton.me>
2025-03-20 01:00:15 +0000

    version 0.1.8

--- inst/doc/conleyreg_introduction.R
+++ inst/doc/conleyreg_introduction.R
@@ -1,18 +1,18 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ---- out.width = "98%", fig.align = "center", fig.cap = "Dense and Sparse Matrix Sizes", echo = FALSE----
+## ----out.width = "98%", fig.align = "center", fig.cap = "Dense and Sparse Matrix Sizes", echo = FALSE----
 knitr::include_graphics("matrix_sizes.png")
 
 
-## ---- out.width = "98%", fig.align = "center", fig.cap = "Speed of Element-Wise and Batch Insertion", echo = FALSE----
+## ----out.width = "98%", fig.align = "center", fig.cap = "Speed of Element-Wise and Batch Insertion", echo = FALSE----
 knitr::include_graphics("matrix_bctimes.png")
 
 
-## ---- out.width = "46%", fig.show = "hold", fig.align = "center", fig.cap = "Weights According to Different Kernels", echo = FALSE----
+## ----out.width = "46%", fig.show = "hold", fig.align = "center", fig.cap = "Weights According to Different Kernels", echo = FALSE----
 knitr::include_graphics("matrix_bartlett.png")
 knitr::include_graphics("matrix_uniform.png")
 
